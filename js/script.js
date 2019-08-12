c0d5d8310cb65b00c2c6ee1c9851e540520766c2
Anna S <bamguanat@yandex.ru>
2019-08-13 01:08:29 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,65 @@
+//focus on name field
+$("#name").focus();
+//hide 'other' input field
+$("form #other-title").css('display', 'none');
+//add option with a message to color selection
+$('#color').prepend($('<option value="selection">Please select a T-shirt theme</option>'));
+
+function hideColorsAndShowMessage () {
+//hide all the options in color select
+$("#color").each (function(element) {
+    $(this).hide();
+  });
+//add option that says to choose the design
+  $('#colors-js-puns').append('<select id="remove-me-later"><option value="no_design_message">Please select a T-shirt theme</option><select>');
+}
+  
+//when something is chosen from the "Design" menu, "Color" appears normally again
+function showColors() {
+  hideColorsAndShowMessage();
+  $("#design").change(function() {   //change in 'design' field
+  $('#remove-me-later').remove();    //remove the 'select theme' message 
+
+      if ($('#design').val() !== "Select Theme") {       //if chosen design value is a real design an not 'select theme' message
+      $("#color").each (function(element) {    
+        $(this).show();                                   //options are shown
+           
+      });
+    } else {
+        hideColorsAndShowMessage();
+    }
+  });
+}
+
+  function showJSPunsColors (){
+ 
+  $('#color [value=tomato]').hide();
+  $('#color [value=steelblue]').hide();
+  $('#color [value=dimgrey]').hide();
+
+  $('#color [value=cornflowerblue]').show();
+  $('#color [value=darkslategrey]').show();
+  $('#color [value=gold]').show();
+
+}
+
+function showILoveJSColors () {
+
+  $('#color [value=cornflowerblue]').hide();
+  $('#color [value=darkslategrey]').hide();
+  $('#color [value=gold]').hide();
+
+  $('#color [value=tomato]').show();
+  $('#color [value=steelblue]').show();
+  $('#color [value=dimgrey]').show();
+}
+
+$('#design').on ('change', () => {
+  if ($( "#design option:selected" ).val() == 'js puns') { 
+    showJSPunsColors();
+  } else if ($( "#design option:selected" ).val() == 'heart js') {
+    showILoveJSColors();
+  }
+})
+  
+
