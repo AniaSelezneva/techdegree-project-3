5b9252948b4817a6c225da7168bfe697b6119e97
Anna S <bamguanat@yandex.ru>
2019-08-14 19:56:23 +0300

    Update script.js
    finished payment and activity

--- js/script.js
+++ js/script.js
@@ -62,98 +62,93 @@ $('#design').on ('change', () => {
   }
 })
 
-// reference each checkbox input, as well as the cost, and day and time
-// from each input’s parent `label` element, and store those values in variables, or in an object as
-// key value pairs.
-
-// const $activities = $('.activities input');  //put all the activities into a collection
-// const checkboxCostValues = [];  //array of prices
-// const checkboxInputValues = [];  //array of checkboxes' input values
-
-
-// const totalCost = 0;
-// for ( let i = 0; i< checkboxCostValues.length; i++) {
-//   totalCost += checkboxCostValues(i);
-// }
-
-// //console.log(totalCost);
-
-// // $activities.each(function(index,element) {
-// //   const cost = document.querySelectorAll('.activities input')[index].getAttribute("data-cost");  
-// //   console.log(x);
-// // })
-
-
-
-
-// for ( let i = 0; i < $activities.length; i ++) {   //loop through activities collection
-//   checkboxInputValues.push($activities[i].value);    //add values of each activity to the checkboxInputValues array
-//   const cost = document.querySelectorAll('.activities input')[i].getAttribute("data-cost");  //get attribute 'cost' content
-//   //let num = parseInt(cost, 10);
-//   checkboxCostValues.push(num);   //add each cost to the array of costs
-// }
-
-// console.log(checkboxCostValues);
-
-
-// Create a DOM element, store it in a global variable and append it to the `.activity` section. You
-// can view the elements tab in the Chrome DevTools to check that your element is in the DOM.
-// Create a global variable to store total activity cost — initially set to 0 — don't use const since
-// you want to update this as needed.
 
 const totalCostElement = '<h2></h2>';
 let totalCost = 0;
 $('.activities').append(totalCostElement);   //append new element to the dom
-let selectedDayAndTime = ' ';  //here we store date and time of checked element
 
-
-// Add a change event listener to the activity section. Inside the listener, it will be helpful to have
-// a variable to reference the DOM `input` element that was just clicked.
 
 $('.activities').change(function (event) {      //event handler for change events in activities section
-  let cost = $(event.target).attr("data-cost");   //get value of attribute data-cost, which is cost
+  let $clicked = $(event.target);       //checkbox that triggered the event
+  let cost = $clicked.attr("data-cost");              //get value of attribute data-cost, which is cost
   cost = cost.match(/\d+/g);         //removing $ sign, leaving just a number
-  cost = parseInt(cost, 10);
+  cost = parseInt(cost, 10);         //this ia all dealing with the cost
 
-  let isChecked = $(event.target).is(':checked');   //isChecked stores true if target is checked
+
+  let $selectedDayAndTime = $clicked.attr("data-day-and-time");    //here we store date and time of checked element   ???????????????????????????????????????????
+  let isChecked = $(event.target).is(':checked');   //isChecked is a bool stores true if target is checked
 
   if(isChecked) {
      totalCost += cost;
-     selectedDayAndTime = $(event.target).attr("data-day-and-time");   //set date and time to the variable above
-     //console.log(selectedDayAndTime);
   } else {
     totalCost -= cost;
   }
   $('h2').text (`Total: $ ${totalCost}`); //add text with total cost to h2 created above
-
   
-})
+  const inputs = document.querySelectorAll('.activities input');  //array of all activities input that exist in the list
+  //ocument.querySelectorAll(".activities input").checked = false;
+  
+  //loop through all activity checkboxes array
+  for(let i = 0; i < inputs.length; i++) {
+    
+       let currentDayAndTime = inputs[i].getAttribute("data-day-and-time");  //current day and time attribute
 
-// First, does the activity occur at the same day and time as the activity that was just
-// clicked? We can check this by seeing if the activity in the current loop iteration has a
-// `data-day-and-time` attribute that is equal to the `data-day-and-time` attribute of the
-// element that was just clicked.
-// ● Second, is the activity is different than the activity that was just clicked? We can check
-// this by seeing if the activity that was just clicked is not equal to the activity in the
-// current loop iteration.
+       if (currentDayAndTime === $selectedDayAndTime) { //if currently chosen dayAndTime matches any in the array
+          //
+       if (isChecked) {
+          //      If the clicked activity was checked, then set the matching activity element's `disabled`
+  //property to `true`
+          inputs[i].disabled = true;   //hide looped checkbox in activity
+          //console.log(selectedDayAndTime);
+        $clicked.prop('disabled',false);      //show clicked item
+      } else if (!isChecked){
+//      If the clicked activity was unchecked, then set the matching activity element's `disabled`
+//property to `false`.
+          inputs[i].disabled = false;
+          $clicked.prop('disabled',false);    //hide clicked item
+      }
+      
+     }
+   }
+  })
 
-const array = document.querySelectorAll('.activities input');
+$('p').hide();     //hide addictional payment information 
 
 
-// $.each( $('.activities input'), function( index, value ){
-//     const currentDayAndTime = $(this).attr("data-day-and-time");
-//     if (currentDayAndTime.toUpperCase() === selectedDayAndTime.toUpperCase()) {
-//       console.log('match');
-//     }
-//     // console.log(typeof currentDayAndTime);
-//     // console.log(typeof selectedDayAndTime);
-//   })
-//   //console.log(currentElement);
 
-  for(let i = 1; i < array.length; i++) {
-    const currentDayAndTime = array[i].getAttribute("data-day-and-time");
-    currentDayAndTime.toString();
-    currentDayAndTime.toLowerCase();
-    console.log(currentDayAndTime);
-    //console.log(typeof selectedDayAndTime);
+$("#payment").change(function() {   //here is a change in 'design' field
+
+  $( '#payment [value="select method"]' ).hide();   //hide the 'select payment method' option
+  
+  const chosenPayment = this.value;      //get value of chosen payment method, it is chosenPayment
+  const $creditCard = $('#credit-card');   //get a hold of credit card div
+  const $paypal = $('p').eq(0);
+  const $bitcoin = $('p').eq(1);
+  
+  //console.log($payPal);
+  if ( chosenPayment === 'paypal' ) {
+    $creditCard.hide();
+    $bitcoin.hide();
+    $paypal.show();
+
+  } else if ( chosenPayment === 'bitcoin') {
+    $creditCard.hide();
+    $paypal.hide();
+    $bitcoin.show();
+
+  }else {
+    $paypal.hide();
+    $bitcoin.hide();
+    $creditCard.show();
+
   }
+ 
+});
+
+
+
+
+
+
+
+
